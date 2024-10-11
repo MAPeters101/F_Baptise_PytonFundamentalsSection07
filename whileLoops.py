8823b83cd9ffdbefa5d31d40138f545a4bec5b58
Mark Peters <mapeters101@earthlink.net>
2024-10-11 16:41:47 -0400

    Lesson074:  Work with while loops to 8:30.

---
+++ whileLoops.py
@@ -0,0 +1,28 @@
+price = 100
+
+while price > 90:
+    print(f'price = {price} - waiting for price to come down...')
+    price -= 1
+print(f'buying at {price}')
+
+price = 100
+while price < 50:
+    print(f'price={price}')
+print('Done')
+
+# price = 100
+# while price > 50:
+#     print(price)
+#     price += 1
+# print('Done')
+
+data = [100, 200, 300, 400, 500]
+while len(data) > 0:
+    last_element = data.pop()
+    print(f'Processing element: {last_element}')
+
+
+
+
+
+
