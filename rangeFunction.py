73c623120a1d2cb4dd9d8bf9a33e7165e97547f5
Mark Peters <mapeters101@earthlink.net>
2024-10-11 13:22:28 -0400

    Lesson070:  Work with the range function

---
+++ rangeFunction.py
@@ -0,0 +1,15 @@
+print(range(10))
+print(type(range(10)))
+print(list('abc'))
+print(list(range(10)))
+print(tuple('abc'))
+print(tuple(range(10)))
+print(list(range(3, 9)))
+print(len(range(3, 9)))
+print(len(range(10)))
+print(list(range(2, 10, 2)))
+print(list(range(2, 11, 2)))
+
+
+
+
